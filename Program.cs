7c187dd43f1d0ae7a764dd25256f4258e018315d
ruoce <sky_ruocen@qq.com>
2025-08-29 22:34:40 +0800

    更新主函数以支持命令行参数，允许用户在启动时指定要打开的文件。调整了 `Form1` 的构造函数以接收文件路径参数。

--- Program.cs
+++ Program.cs
@@ -6,12 +6,17 @@ namespace PdfCutter
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            string? fileToOpen = null;
+            if (args != null && args.Length > 0)
+            {
+                fileToOpen = args[0];
+            }
+            Application.Run(new Form1(fileToOpen));
         }
     }
 }
